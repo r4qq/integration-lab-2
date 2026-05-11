b6600b6c9369c64c91547f15a040ccf84e1753c2
r4qq <r4qq@protonmail.com>
2026-05-11 13:58:47 +0200

    Initialize database and basic administrative access

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -24,6 +27,48 @@ type User struct {
 	gorm.Model        // dodaje automatycznie pola id, createdAt, updatedAt, deletedAt
 	Username   string `gorm:"unique;not null" form:"user"`
 	Posts      []Post
+}
+
+// helper do generowania połączenia z bazą
+func setupDatabase() *gorm.DB {
+	var err error
+	var db *gorm.DB
+
+	db, err = gorm.Open(sqlite.Open("posts.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("connection to db failed: %v", err)
+	}
+
+	err = db.AutoMigrate(&User{}, &Post{})
+	if err != nil {
+		log.Fatalf("migartion to db failed: %v", err)
+	}
+
+	// przykladowi userzy
+	var users = []User{
+		{Username: "glenda"},
+		{Username: "pike"},
+	}
+
+	var count int64
+	db.Model(&User{}).Count(&count)
+	if count == 0 {
+		db.Create(&users)
+		log.Println("users seeded")
+	}
+
+	// przykładowe posty
+	var posts = []Post{
+		{Title: "9front", Body: "the front fell off", UserID: users[0].ID},
+		{Title: "go", Body: "lang", UserID: users[1].ID},
+	}
+
+	db.Model(&Post{}).Count(&count)
+	if count == 0 {
+		db.Create(&posts)
+		log.Println("users seeded")
+	}
+	return db
 }
 
 func main() {
